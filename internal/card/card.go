433678dc06be65c332a6417e7a5eb0f2b16328ba
tomonakar <tomohisa.nak@gmail.com>
2022-01-06 18:14:10 +0900

    feat: stripeのpaymentIntent処理を追加

---
+++ internal/card/card.go
@@ -0,0 +1,62 @@
+package card
+
+import (
+	"github.com/stripe/stripe-go/v72"
+	"github.com/stripe/stripe-go/v72/paymentintent"
+)
+
+type Card struct {
+	Secret   string
+	Key      string
+	Currency string
+}
+
+type Transaction struct {
+	TransactionStatusID int
+	Amount              int
+	Currency            string
+	LastFour            string
+	BankReturnCode      string
+}
+
+func (c *Card) Charge(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	return c.CreatePaymentIntent(currency, amount)
+}
+
+func (c *Card) CreatePaymentIntent(currency string, amount int) (*stripe.PaymentIntent, string, error) {
+	stripe.Key = c.Secret
+
+	// create a payment intent
+	params := &stripe.PaymentIntentParams{
+		Amount:   stripe.Int64(int64(amount)),
+		Currency: stripe.String(currency),
+	}
+
+	// トランザクションに情報を追加する場合はこのようにする
+	params.AddMetadata("key", "value")
+
+	pi, err := paymentintent.New(params)
+	if err != nil {
+		msg := ""
+		if stripeErr, ok := err.(*stripe.Error); ok {
+			msg = cardErrMessage(stripeErr.Code)
+		}
+		return nil, msg, err
+	}
+	return pi, "", nil
+}
+
+func cardErrMessage(code stripe.ErrorCode) string {
+	var msg = ""
+	switch code {
+	case stripe.ErrorCodeCardDeclined:
+		msg = "カードが拒否されました"
+	case stripe.ErrorCodeExpiredCard:
+		msg = "カードの有効期限が切れています"
+	default:
+		msg = "カードが拒否されました"
+
+	}
+
+	return msg
+}
